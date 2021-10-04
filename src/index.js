5ecd4214dd5a38b65b0092d03f70ec794875db12
MinusculeOpulentus <79850005+MinusculeOpulentus@users.noreply.github.com>
2021-10-04 14:28:07 +0900

    Start express app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Working...")
+const express = require('express');
+
+const app = express();
+
+app.all('/', (req, res) => {
+    res.write('It is working...');
+    res.end();
+});
+
+app.listen(5000, console.log.bind(console, 'App is running on http://localhost:5000'));
