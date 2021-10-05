5a52cd2ede5e28555ec67f0b52fe973a6feda9b8
MinusculeOpulentus <79850005+MinusculeOpulentus@users.noreply.github.com>
2021-10-05 21:10:37 +0900

    Browse dynamic cubes

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,10 +1,38 @@
+const uniqid = require('uniqid')
+
 class Cube {
+    static #cubes = [
+        {   
+            id: 'abc',
+            name: 'Mirror Cube',
+            description: 'Strange cube',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Mirror_Cube_solved.png/1200px-Mirror_Cube_solved.png',
+            difficulty: '4'
+        },
+        {
+            id: 'abcef',
+            name: 'Ice Cube',
+            description: 'US Rapper',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Ice-Cube_2014-01-09-Chicago-photoby-Adam-Bielawski.jpg',
+            difficulty: '1'
+
+        }
+    ];
     constructor(name, description, imageUrl, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
-    }
+    };
+
+    static getAll() {
+        return Cube.#cubes.slice();
+    };
+
+    static add(cube) {
+        Cube.#cubes.push(cube);
+    };
 }
 
 module.exports = Cube;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,22 +1,15 @@
 const Cube = require('../models/Cube');
-const cubeDb = [
-    {
-    name: 'Mirror Cube',
-    description: 'Strange cube',
-    imageUrl: 'https://www.ecosia.org/images?q=mirror%20cube%20rubik#id=CF1633E2506FB67E403CC320584D54E47F1ACB1D',
-    difficulty: '4'
-    }
-];
 
-const getAll = () => cubeDb.slice();
+
+const getAll = () => Cube.getAll();
 
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube(name, description, imageUrl, difficulty)
-    cubeDb.push(cube);
+    Cube.add(cube);
 };
 
-const cubeService = {
-    create, 
-    getAll
+const cubeService = { 
+    getAll, 
+    create,
 };
 module.exports = cubeService;
