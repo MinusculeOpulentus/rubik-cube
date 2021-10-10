60121f1c4b37f651792e67d8f43969ab12f4c2f8
MinusculeOpulentus <79850005+MinusculeOpulentus@users.noreply.github.com>
2021-10-10 22:38:29 +0900

    Attached filtered accessories

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -6,11 +6,17 @@ async function create(name, imageUrl, description) {
 
 async function getAll() {
     return Accessory.find({}).lean()
+};
+
+async function getRemaining(accessoryId) {
+    return Accessory.find({_id: {$nin: accessoryId}}).lean();
+
 }
 
 const accessoryService = { 
     create,
     getAll,
+    getRemaining,
     
 };
 module.exports = accessoryService;
